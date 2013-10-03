75cd1d3bb7484347483bbddf5db4d62c10a76e3e
Jáchym Toušek <enumag@gmail.com>
2013-10-03 09:09:56 +0200

    Replaced dependencies with suggest

--- src/Arachne/Doctrine/DI/DoctrineExtension.php
+++ src/Arachne/Doctrine/DI/DoctrineExtension.php
@@ -21,23 +21,29 @@ class DoctrineExtension extends CompilerExtension
 			->setClass('Doctrine\Common\Persistence\ManagerRegistry')
 			->setFactory('Arachne\Doctrine\ManagerRegistry');
 
-		$builder->addDefinition($this->prefix('validatorListener'))
-			->setClass('Arachne\Doctrine\ValidatorListener')
-			->addTag(EventsExtension::SUBSCRIBER_TAG);
+        if (class_exists('Kdyby\Events\DI\EventsExtension')) {
+			$builder->addDefinition($this->prefix('validator.validationListener'))
+				->setClass('Arachne\Doctrine\Validation\ValidatorListener')
+				->addTag(EventsExtension::SUBSCRIBER_TAG);
+		}
 
-		$builder->addDefinition($this->prefix('typeGuesser'))
-			->setClass('Symfony\Component\Form\FormTypeGuesserInterface')
-			->setFactory('Symfony\Bridge\Doctrine\Form\DoctrineOrmTypeGuesser')
-			->addTag(FormsExtension::TAG_TYPE_GUESSER);
-			
-		$builder->addDefinition($this->prefix('initializer'))
-			->setFactory('Symfony\Bridge\Doctrine\Validator\DoctrineInitializer')
-			->addTag(ValidatorExtension::TAG_INITIALIZER);
+        if (class_exists('Kdyby\Validator\DI\ValidatorExtension')) {
+			$builder->addDefinition($this->prefix('validator.initializer'))
+				->setFactory('Symfony\Bridge\Doctrine\Validator\DoctrineInitializer')
+				->addTag(ValidatorExtension::TAG_INITIALIZER);
+		}
 
-		$builder->addDefinition($this->prefix('type.entity'))
-			->setClass('Symfony\Bridge\Doctrine\Form\Type\EntityType')
-			->addTag(FormsExtension::TAG_TYPE, 'entity')
-			->setAutowired(FALSE);
+		if (class_exists('Arachne\Forms\DI\FormsExtension')) {
+			$builder->addDefinition($this->prefix('forms.typeGuesser'))
+				->setClass('Symfony\Component\Form\FormTypeGuesserInterface')
+				->setFactory('Symfony\Bridge\Doctrine\Form\DoctrineOrmTypeGuesser')
+				->addTag(FormsExtension::TAG_TYPE_GUESSER);
+
+			$builder->addDefinition($this->prefix('forms.type.entity'))
+				->setClass('Symfony\Bridge\Doctrine\Form\Type\EntityType')
+				->addTag(FormsExtension::TAG_TYPE, 'entity')
+				->setAutowired(FALSE);
+		}
 	}
 
 }
