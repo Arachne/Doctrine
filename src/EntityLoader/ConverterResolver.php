308f63c81dc73aba0e79e2698c4899a1b183f696
Jáchym Toušek <enumag@gmail.com>
2015-01-26 13:55:13 +0100

    Fixed ConverterResolver

--- src/EntityLoader/ConverterResolver.php
+++ src/EntityLoader/ConverterResolver.php
@@ -2,18 +2,22 @@
 
 namespace Arachne\Doctrine\EntityLoader;
 
+use AppendIterator;
+use ArrayIterator;
 use Arachne\DIHelpers\ResolverInterface;
+use Iterator;
+use IteratorAggregate;
 use Nette\Object;
 
 /**
  * @author Jáchym Toušek <enumag@gmail.com>
  */
-class ConverterResolver extends Object implements ResolverInterface
+class ConverterResolver extends Object implements IteratorAggregate, ResolverInterface
 {
 
 	/** @var ResolverInterface */
 	private $resolver;
-	
+
 	/** @var DoctrineConverter */
 	private $converter;
 
@@ -27,9 +31,20 @@ class ConverterResolver extends Object implements ResolverInterface
 	 * @param string $name
 	 * @return object
 	 */
-	public function __invoke($name)
+	public function resolve($name)
 	{
 		return $this->resolver->resolve($name) ?: ($this->converter->entityExists($name) ? $this->converter : NULL);
+	}
+
+	/**
+	 * @return Iterator
+	 */
+	public function getIterator()
+	{
+		$iterator = new AppendIterator();
+		$iterator->append($this->resolver->getIterator());
+		$iterator->append(new ArrayIterator([ $this->converter ]));
+		return $iterator;
 	}
 
 }
