4f3c76497e69582d6cc3d8847e6d5745250997cc
Jáchym Toušek <enumag@gmail.com>
2015-04-12 12:47:53 +0200

    Added integration tests

---
+++ tests/integration/src/Classes/ArticleQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Integration\Classes;
+
+use Arachne\Doctrine\EntityLoader\QueryInterface;
+use Doctrine\ORM\EntityRepository;
+use Exception;
+
+class ArticleQuery implements QueryInterface
+{
+
+	public function getEntity(EntityRepository $repository)
+	{
+		try {
+			$qb = $repository->createQueryBuilder('a');
+			return $qb->where($qb->expr()->like('a.name', ':name'))
+				->setParameter('name', '%ipsum%')
+				->getQuery()
+				->getSingleResult();
+		} catch (Exception $e) {
+		}
+	}
+
+}
